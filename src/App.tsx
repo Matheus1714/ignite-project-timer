0847906b13c7a7a258178fc0febd4dd829f3c3fb
matheus1714 <matheussilvamartins1714@gmail.com>
2023-08-13 20:00:53 -0300

    eslint configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { Button } from "./Button"
-import { ThemeProvider } from "styled-components"
-import { defaultTheme } from "./styles/themes/default"
+import { Button } from './Button'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
       <Button variant="secondary" />
       <Button variant="success" />
       <Button variant="danger" />
-      <Button/>
+      <Button />
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
